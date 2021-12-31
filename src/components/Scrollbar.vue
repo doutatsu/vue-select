399b9bbe6e0880db8e8967fd2a32b3567e2a958f
doutatsu <doutatsu@gmail.com>
2021-12-31 20:54:18 +0000

    Add overlay-scrollbars

---
+++ src/components/Scrollbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div ref="element" tabindex="0">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup>
+import 'overlayscrollbars/css/OverlayScrollbars.css'
+import OverlayScrollbars from 'overlayscrollbars'
+import { onMounted, onUnmounted, ref } from 'vue'
+
+let instance
+const element = ref(null)
+
+onMounted(() => {
+  instance = OverlayScrollbars(element.value, {
+    scrollbars: { autoHide: 'move' },
+  })
+})
+
+onUnmounted(() => {
+  instance.destroy()
+})
+</script>
+
+<style lang="scss">
+.os-viewport {
+  overscroll-behavior-block: contain;
+}
+</style>
